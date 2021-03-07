c06addedb9b2bedd11322e9a349aa9c92d2d55e7
Timur ramazanov <37983667+Takerrim@users.noreply.github.com>
2021-03-07 19:55:27 +0400

    Refactor formatTime func

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,5 @@
 export function formatTime(time) {
   const minutes = Math.floor(time / 60)
   const seconds = time % 60
-  const decorativeMinutes = minutes < 10 ? `0${minutes}` : minutes
-  const decorativeSeconds = seconds < 10 ? `0${seconds}` : seconds
-  return `${decorativeMinutes}:${decorativeSeconds}`
+  return `${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}`
 }
